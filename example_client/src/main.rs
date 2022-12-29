f7fada8998d929428d685488dd74d6b5e6421666
Kevin Graney <kmg@google.com>
2022-12-28 21:45:34 -0500

    Add example_client support for fake NSM docs

--- example_client/src/main.rs
+++ example_client/src/main.rs
@@ -6,6 +6,9 @@ use tokio::io::{stdout, AsyncWriteExt as _};
 
 #[derive(Parser, Debug)]
 struct CliArgs {
+    #[arg(long)]
+    no_nsm: bool,
+
     fetch_url: String,
 }
 
@@ -15,12 +18,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     openssl_probe::init_ssl_cert_env_vars();
 
     let args = CliArgs::parse();
-    log::info!("Requesting page from: {}", args.fetch_url);
+    let verifier = match args.no_nsm {
+        true => Verifier::new_fake(),
+        false => Verifier::new_aws(),
+    };
 
-    let verifier = Verifier::new_aws();
     let client_builder = AttestedBuilder::new(verifier);
     let client = client_builder.http_client()?;
 
+    log::info!("Requesting page from: {}", args.fetch_url);
     let mut resp = client.get(args.fetch_url.parse()?).await?;
     log::info!("Response: {}", resp.status());
     while let Some(chunk) = resp.body_mut().data().await {
